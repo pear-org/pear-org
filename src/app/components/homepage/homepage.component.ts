414926a812dbfb32b0ef95548695dd1a36c5ebf7
Yip <crystal.yip@cgi.com>
2019-08-27 22:27:03 -0400

    modified homepage

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import Jobposting from '../../models/jobposting.model';
-
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'public',
-  templateUrl: './homepage.component.html'
+  templateUrl: './homepage.component.html',
+  providers: [NgbCarouselConfig]
 })
 export class PublicComponent implements OnInit{
-  constructor() { }
+  showNavigationArrows = true;
+  constructor(config: NgbCarouselConfig) {
+    config.showNavigationArrows = true;
+  }
 
   public newJobposting: Jobposting = new Jobposting()
 
   jobPostingList: Jobposting[];
 
   ngOnInit(): void {
-    
+
   }
   title = 'PearAngular';
 }
