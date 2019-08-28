689c3b33527105626e546ca623395ca970853b5d
crystalyip <crystal.yip@mail.utoronto.ca>
2019-08-27 22:28:15 -0400

    modified homepage

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import Jobposting from '../../models/jobposting.model';
-
+import * as $ from 'jquery';
+import ScrollSnap from 'scroll-snap';
 
 @Component({
   selector: 'public',
@@ -14,7 +15,20 @@ export class PublicComponent implements OnInit{
   jobPostingList: Jobposting[];
 
   ngOnInit(): void {
+    const snapConfig = {
+      scrollSnapDestination: '90% 0%', // *REQUIRED* scroll-snap-destination css property, as defined here: https://developer.mozilla.org/en-US/docs/Web/CSS/scroll-snap-destination
+      scrollTimeout: 100, // *OPTIONAL* (default = 100) time in ms after which scrolling is considered finished
+      scrollTime: 300, // *OPTIONAL* (default = 300) time in ms for the smooth snap
+    }
     
+    function callback() {
+      alert('called when snap animation ends');
+    }
+    
+    const element = document.getElementById('testsnap');
+    const snapObject = new ScrollSnap(element, snapConfig);
+    
+    snapObject.bind(callback);
   }
   title = 'PearAngular';
 }
