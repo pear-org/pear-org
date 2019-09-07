21d74ef71de05f135534b0af82f6ecf03d21545a
crystalyip <crystal.yip@mail.utoronto.ca>
2019-09-06 21:02:13 -0400

    added animations

--- src/app/app.component.header.ts
+++ src/app/app.component.header.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-navbar',
     templateUrl: './app.component.header.html'
 })
 export class AppComponentHeader implements OnInit{
-  constructor(
-  ) { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
-    if ($(this).parents('.nav-menu').length) {
+    if (this.router.url === '/') {
+      console.log('active');
       $('.nav-menu .menu-active').removeClass('menu-active');
-      $(this).closest('li').addClass('menu-active');
     }
 
   }
